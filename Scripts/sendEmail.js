acb121d6cf03c2bf31649c9c0b590e5521cd51ed
matt-45 <53614616+matt-45@users.noreply.github.com>
2019-11-26 15:06:48 -0500

    Changed Bio

---
+++ Scripts/sendEmail.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    //640608a9-9cc9-4928-89b9-ba7170d9e216
+
+    $("#submitButton").on('click', function () {
+        var userEmail = $("#email").val();
+        var userMessage = $("#message").val();
+        var userName = $("#name").val()
+        console.log(userName)
+        console.log(userEmail)
+        console.log(userMessage)
+        Email.send({
+            SecureToken: "168159af-9124-4e40-b207-2026d7c66e77",
+            To: "mattpark102@outlook.com",
+            From: "mattpark102@outlook.com",
+            Subject: "Message from portfolio.",
+            Body: "Name:" + userName + "Email:" + userEmail + "Message" + userMessage
+        }).then(
+            message => alert(message)
+        );
+
+    })
+});
